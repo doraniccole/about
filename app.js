dcb57b5029a72a3c82fc4ce63771956288ed1472
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-09-02 03:38:59 -0600

    adding assignment

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+console.log('js is loaded');
+
+// Here is a string 
+var name = 'What is your name'; 
+
+// getting input from a sign
+var name = prompt('What is your name?');
+
+alert('Great meeting you, ' + name);
+console.log(name);
+console.log('Great meeting you, ' + name);
+
+// below are notes
+
+var address = confirm('How would you like to be addressed?');
+var answer1 = any;
+
+console.log(address);
+
+if ( === true) {
+    answer1 = prompt('It is refreshing to be able to do so.');
+}
+
+var outdoors = confirm('Have you been outdoors today?');
+var answer1 = yes 
+var answer2 = no 
+.toLowerCase()
+// will continue and clean this up for function
+
+
+// crafted and brewed by dora with Jacob's code 201 guidance
+// codeproject.com credit to 'How to Adjust your WebSite to fit all types of Resolution'
+// credit W3School.com for google fonts direction
+
+How may I address you?
+Have you been outdoors today? 
+Do you have human children?
+Are your earlobes connected?
+Do you play video games?
+Do you sing along with the music playing in the background?
