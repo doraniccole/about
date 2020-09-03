5bbc78777a49bbea4e8f18c34b81a22ae60fa434
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-09-03 04:00:42 -0600

    modifying file

--- app.js
+++ app.js
@@ -3,40 +3,82 @@
 console.log('js is loaded');
 
 // Here is a string 
-var name = 'What is your name'; 
+var name = 'How would you like to be addressed?'; 
 
 // getting input from a sign
-var name = prompt('What is your name?');
+var name = prompt('How would you like to be addressed?');
 
 alert('Great meeting you, ' + name);
 console.log(name);
 console.log('Great meeting you, ' + name);
 
-// below are notes
+// here is a string
+var outdoors = 'Have you been outdoors today?';
 
-var address = confirm('How would you like to be addressed?');
-var answer1 = any;
+// getting input
+var outdoors = prompt('Have you been outdoors today?');
+    if (outdoors.toLowerCase() === 'yes') {
+    alert('Great! Correct.');
+} else if (outdoors.toLowerCase() === 'no') {
+    alert('Oops! Incorrect.');
+        }
 
-console.log(address);
+var kids = 'Do you have human children?';
 
-if ( === true) {
-    answer1 = prompt('It is refreshing to be able to do so.');
-}
+var kids = prompt('Do you have human children?');
+    if (kids.toLowerCase() === 'yes') {
+    alert('Absolutely. Correct.');
+} else if (kids.toLowerCase() === 'no') {
+    alert('Oops! Incorrect.');
+        }
 
-var outdoors = confirm('Have you been outdoors today?');
-var answer1 = yes 
-var answer2 = no 
-.toLowerCase()
-// will continue and clean this up for function
+var desert = 'Have you been to the desert?';
+
+var desert = prompt('Have you been to the desert?');
+    if (desert.toLowerCase() === 'yes') {
+        alert('Fantastic! Correct.');
+ } else if (desert.toLowerCase() === 'no') {
+        alert('Darn. Incorrect.');
+        }
+
+var sing = 'Do you like to sing?';
+
+var sing = prompt('Do you like to sing?');
+    if (sing.toLowerCase() === 'yes') {
+        alert('Fabulous. Correct.'); 
+ } else if (sing.toLowerCase() === 'no') {
+        alert('Incorrect.');
+        }
+
+var teach = 'Have you ever taught?';
+
+var teach = prompt('Have you ever taught?');
+     if (teach.toLowerCase() === 'yes') {
+            alert('Excellent. Correct.');
+  } else if (teach.toLowerCase() === 'no') {
+            alert('Try Again. Incorrect.');
+        }
+
+    
+var secretNumber = 8;
+var tries = 0;
+var guess;
+    while (tries < 6) {
+        tries++
+        var guess = prompt('Guess a number between 1-10');
+        if (guess < 8) {
+            alert('too low');
+        } else if (guess > 8) {
+            alert('too high');
+        }
+        else if (guess == 8) {
+            alert('You guessed Correct!');
+        }
+    }
+    alert('Number of guesses expired. The secret number is ' + secretNumber) 
 
 
-// crafted and brewed by dora with Jacob's code 201 guidance
+// crafted and brewed by dora with Jacob's Code 201 guidance
 // codeproject.com credit to 'How to Adjust your WebSite to fit all types of Resolution'
 // credit W3School.com for google fonts direction
 
-How may I address you?
-Have you been outdoors today? 
-Do you have human children?
-Are your earlobes connected?
-Do you play video games?
-Do you sing along with the music playing in the background?
